631479d4070be4dde983b9ae6b637fad1ade358e
Enrique Pérez Arnaud <enrique@cazalla.net>
2026-02-11 16:34:12 +0100

    migration scripts

--- migration-scripts/wrapmd.sh
+++ migration-scripts/wrapmd.sh
@@ -3,6 +3,10 @@
 DIRECTORY=$1
 
 find "$DIRECTORY" -type f -name "*.md" | while IFS= read -r file; do
+    # Skip files that already have front matter delimiters
+    if head -n 1 "$file" | grep -q '^---$'; then
+        continue
+    fi
     awk '
     BEGIN {print "---"; inParagraph=1}
     /^$/ && inParagraph {print "---\n"; inParagraph=0; next}
